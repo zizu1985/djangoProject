c189847cb1c2e34064c02a1142f128d1d53d36b0
root <root@ol7-122-dg1.localdomain>
2019-04-13 18:46:36 +0000

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, \
+				  PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
+# Create your views here.
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,'blog/post/list.html',{'page' : page, 'posts': posts})
+		    
+		    
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+				    status='published',
+				    publish__year=year,
+				    publish__month = month,
+				    publish__day = day)
+    return render(request,'blog/post/detail.html',{'post': post})
+    
+    
+def post_share(request, post_id):
+    # Retrieve post by id
+    # Class name, first parameter, second parameter. Method to get access to model
+    post = get_object_or_404(Post, id=post_id, status='published') 
+    sent = False
+    
+    if request.method == 'POST':
+	# Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+	    # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'],cd['email'],post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title,post.get_absolute_url,cd['name'],cd['comments'])
+            send_mail(subject,message,'tziss85@gmail.com',[cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request,'blog/post/share.html', {'post' : post,'form' : form, 'sent' : sent})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
